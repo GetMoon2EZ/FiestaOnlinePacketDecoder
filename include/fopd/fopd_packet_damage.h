5b3c7b8d997dcd8482999678ca2e98cb05b1f995
GetMoon2EZ <luc.techer@sfr.fr>
2022-08-16 22:21:38 +0800

    add: damage packet class

--- include/fopd/fopd_packet_damage.h
+++ include/fopd/fopd_packet_damage.h
@@ -1,17 +1,50 @@
 #ifndef __FOPD_PACKET_DAMAGE_H__
 #define __FOPD_PACKET_DAMAGE_H__
 
-#include <cstdint>
-
 #include "fopd/fopd_packet.h"
+
+#include <cstdint>
+#include <iostream>
+
+#include <vector>
+
+#include <fopd/fopd_types.h>
+#include <fopd/fopd_consts.h>
+
+
+#define FOPD_DAMAGE_PACKET_PAYLOAD_MIN_LEN  (25 - FOPD_PACKET_HEADER_LEN)
+
+// Damage value
+#define DAMAGE_VALUE_OFFSET                 (15 - FOPD_PACKET_HEADER_LEN)
+#define DAMAGE_VALUE_LEN                    4
+
+// Remaining health
+#define TARGET_REMAINING_HEALTH_OFFSET      (DAMAGE_VALUE_OFFSET + DAMAGE_VALUE_LEN)
+#define TARGET_REMAINING_HEALTH_LEN      4
 
 class FiestaOnlinePacketDamage: public FiestaOnlinePacket
 {
 private:
+    uint32_t damage_value = 0;
+    uint32_t target_remaining_health = 0;
+
+    fopd_status_t parsePayload(void);
+
 public:
-    FiestaOnlinePacketDamage(uint8_t *data) {
-        // TODO
+    FiestaOnlinePacketDamage(uint8_t *data)
+    :FiestaOnlinePacket(data) {
+        parsePayload();
     }
+
+    FiestaOnlinePacketDamage(std::vector<uint8_t> data)
+    :FiestaOnlinePacket(data) {
+        parsePayload();
+    }
+
+    uint32_t getDamageValue(void) const;
+    uint32_t getTargetRemainingHealth(void) const;
+
+    friend std::ostream& operator<<(std::ostream& out, FiestaOnlinePacketDamage& pkt);
 };
 
 #endif // __FOPD_PACKET_DAMAGE_H__
--- src/packets/damage.cpp
+++ src/packets/damage.cpp
@@ -1,3 +1,40 @@
 #include "fopd/fopd_packet_damage.h"
 
+#include <cstdint>
+#include <iostream>
+
 #include "fopd/fopd_types.h"
+
+
+using namespace std;
+
+fopd_status_t FiestaOnlinePacketDamage::parsePayload(void)
+{
+    if (this->payload_len < FOPD_DAMAGE_PACKET_PAYLOAD_MIN_LEN) {
+        cerr << "[ERROR] Cannot parse given data" << endl;
+        return FOPD_ERROR;
+    }
+
+    this->damage_value = little_endian_byte_array_to_uint32(this->payload + DAMAGE_VALUE_OFFSET);
+    this->target_remaining_health = little_endian_byte_array_to_uint32(this->payload + TARGET_REMAINING_HEALTH_OFFSET);
+
+    return FOPD_OK;
+}
+
+uint32_t FiestaOnlinePacketDamage::getDamageValue(void) const
+{
+    return this->damage_value;
+}
+
+uint32_t FiestaOnlinePacketDamage::getTargetRemainingHealth(void) const
+{
+    return this->target_remaining_health;
+}
+
+ostream& operator<<(ostream& out, FiestaOnlinePacketDamage& pkt)
+{
+    out << "Damage packet:" << endl;
+    out << "\t- Damage value: " << pkt.getDamageValue() << endl;
+    out << "\t- Target remaining health: " << pkt.getTargetRemainingHealth() << " HP";
+    return out;
+}
