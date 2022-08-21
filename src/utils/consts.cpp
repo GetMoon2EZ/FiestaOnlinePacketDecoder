c3e34e87f4d1fc8531adbc52d899d981c2d9a36e
GetMoon2EZ <luc.techer@sfr.fr>
2022-08-21 10:54:48 +0800

    refact: const arrays use enums as values

--- src/utils/consts.cpp
+++ src/utils/consts.cpp
@@ -8,5 +8,17 @@
 // Turn into map for release
 const std::string FIESTA_ONLINE_SERVER_ADDRESS = "35.189.254.31";
 
-const uint8_t FOPD_ENTITY_CLICK_PACKET_HEADER[FOPD_PACKET_HEADER_LEN] = { 0x02, 0x24 };
-const uint8_t FOPD_DAMAGE_PACKET_HEADER[FOPD_PACKET_HEADER_LEN] = { 0x52, 0x24 };
+const uint8_t FOPD_SPELL_DAMAGE_PACKET_HEADER[FOPD_PACKET_HEADER_LEN] = {
+                                                                            FOPD_SPELL_DAMAGE_PACKET_HEADER_B0,
+                                                                            FOPD_SPELL_DAMAGE_PACKET_HEADER_B1
+                                                                        };
+
+const uint8_t FOPD_AA_DAMAGE_PACKET_HEADER[FOPD_PACKET_HEADER_LEN]    = {
+                                                                            FOPD_AA_DAMAGE_PACKET_HEADER_B0,
+                                                                            FOPD_AA_DAMAGE_PACKET_HEADER_B1
+                                                                        };
+
+const uint8_t FOPD_ENTITY_CLICK_PACKET_HEADER[FOPD_PACKET_HEADER_LEN] = {
+                                                                            FOPD_ENTITY_CLICK_PACKET_HEADER_B0,
+                                                                            FOPD_ENTITY_CLICK_PACKET_HEADER_B1
+                                                                        };
