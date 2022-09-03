406d61aae475c6c3f33e87b99ad79f9b10e03ccf
GetMoon2EZ <luc.techer@sfr.fr>
2022-09-03 18:09:44 +0200

    chore: cleanup main

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,12 @@
 using namespace std;
 
 int main() {
-    cout << "Starting ... " << endl;
-
     const uint32_t DPS_UPDATE_TIME_MS = 100;
     const uint32_t PING_UPDATE_TIME_MS = 1000;
 
     fopd_damage_queue dmg_q;
 
-    // Start and detach threads to avoid a call to terminate
-
+    // Start and detach each thread to avoid a call to terminate
     thread t1(dps_thread, &dmg_q, DPS_UPDATE_TIME_MS);
     t1.detach();
 
@@ -32,8 +29,8 @@ int main() {
     t3.detach();
     
     // Run the UI in the main thread
+    // Exit whenever the GUI returns (ie: window is closed)
     run_gui();
 
-    // Exit whenever the GUI returns (ie: window is closed)
     return 0;
 }
