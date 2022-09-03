b4efd3d42590406aee44b46a34b3954f103e567f
GetMoon2EZ <luc.techer@sfr.fr>
2022-09-03 18:06:16 +0200

    add: ping thread integration

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@
 #include "fopd/fopd_packet_entity_stats.h"
 #include "fopd/fopd_packet_damage.h"
 #include "fopd/gui.h"
+#include "fopd/ping.h"
 
 
 using namespace std;
@@ -23,15 +24,21 @@ using namespace std;
 int main() {
     cout << "Starting ... " << endl;
 
-    const int DPS_UPDATE_TIME_MS = 100;
+    const uint32_t DPS_UPDATE_TIME_MS = 100;
+    const uint32_t PING_UPDATE_TIME_MS = 1000;
+
     fopd_damage_queue dmg_q;
 
-    thread t1(dps_thread, &dmg_q, DPS_UPDATE_TIME_MS);
-    thread t2(sniffer_thread, &dmg_q);
+    // Start and detach threads to avoid a call to terminate
 
-    // Detach threads to avoid a call to terminate
+    thread t1(dps_thread, &dmg_q, DPS_UPDATE_TIME_MS);
     t1.detach();
+
+    thread t2(sniffer_thread, &dmg_q);
     t2.detach();
+
+    thread t3(ping_thread, PING_UPDATE_TIME_MS);
+    t3.detach();
     
     // Run the UI in the main thread
     run_gui();
